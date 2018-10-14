6cf330b1a147128d799f8e984c214e830189681b
Michael Sklepowich <michael@sklepowich.com>
2018-10-14 12:09:36 -0700

    adding insert before and insert after methods

--- data_structures/linked_lists/linked_list.py
+++ data_structures/linked_lists/linked_list.py
@@ -52,3 +52,43 @@ class LinkedList(object):
             current = current._next
         print(self)    
         return False       
+
+    def insert_before(self, i_data, s_data):
+        '''
+        Add a new node with the given i_data immediately after the first s_data node
+        O(n) time.
+        '''
+        if self._head is not None:
+
+            curr = self._head
+            n = Node(i_data)
+
+            if self._head._data == s_data:
+                n._next, self._head = curr, n
+                self._length += 1
+            else:
+                prev = None
+                while curr:
+                    if curr._data == s_data:
+                        n._next, prev._next = curr, n
+                        self._length += 1
+                    prev, curr = curr, curr._next
+
+    def insert_after(self, i_data, s_data):
+        '''
+        Add a new node with the given i_data immediately after the first s_data node
+        O(n) time.
+        '''
+        if self._head is not None:
+
+            if self._head._data == s_data:
+                self._head._next = Node(i_data, self._head._next)
+                self._length += 1
+
+            else:
+                curr = self._head
+                while curr:
+                    if curr._data == s_data:
+                        curr._next = Node(i_data, curr._next)
+                        self._length += 1
+                    curr = curr._next
